61e4ee19a4f2c2838ff194561b6690f803c37b8d
PeterCcT <pedrorodriguesfereira@gmail.com>
2021-05-23 22:58:08 -0300

    More fixes to deploy

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,6 @@ createDbConnection().then(
 )
 
 app.listen(
-    process.env.SERVER_PORT,
-    () => console.log(`Server running on port ${process.env.SERVER_PORT}`)
+    process.env.PORT,
+    () => console.log(`Server running on port ${process.env.PORT}`)
 )
